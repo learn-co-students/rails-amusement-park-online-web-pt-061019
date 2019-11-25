bf237f3862533c6e6590ccc79311eb2523c89863
maasa333 <konomaasa@gmail.com>
2019-11-24 18:16:32 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_002243) do
+ActiveRecord::Schema.define(version: 2019_11_25_014336) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_002243) do
     t.integer "nausea_rating"
     t.integer "happiness_rating"
     t.integer "min_height"
+    t.integer "user_id"
   end
 
   create_table "rides", force: :cascade do |t|
@@ -26,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_002243) do
     t.integer "nausea_rating"
     t.integer "happiness_rating"
     t.integer "min_height"
+    t.integer "user_id"
+    t.integer "attraction_id"
   end
 
   create_table "users", force: :cascade do |t|
