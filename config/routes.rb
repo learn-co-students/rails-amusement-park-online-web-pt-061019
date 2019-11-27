db113acbd6d2a72ff11f1e3bd8fddeec2a1106ab
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 17:59:38 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+
+  resources :users
+  resources :attractions
+  resources :rides
+
+  get '/signin', to: 'sessions#signin'
+  post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
