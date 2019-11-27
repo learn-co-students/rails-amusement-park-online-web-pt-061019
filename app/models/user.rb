c0646dfe81202de12a5127a7d87e9624012e76a6
scrabill <shannon.crabill@gmail.com>
2019-11-26 20:05:20 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
 
+  validates :name, presence: true
   validates :password, presence: true
+  validates :happiness, presence: true
+  validates :nausea, presence: true
+  validates :height, presence: true
+  validates :tickets, presence: true
+
+
+  # it "is valid with a name, password, happiness, nausea, height, and tickets" do
+
 
   def mood
     if self.happiness > self.nausea
