a0e58c00c6a37415bb48abfc5879ef3a9d51cbe4
Brittany Javalera <brittjavalera@gmail.com>
2019-11-26 21:02:21 -0800

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user_id
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height)
+    end
+end
---
+++ app/helpers/login_helper.rb
@@ -0,0 +1,16 @@
+module LoginHelper
+
+    def user_signup
+    end
+
+    def user_login
+    end
+
+    def admin_signup
+    end
+
+    def admin_login
+    end
+
+    
+end
