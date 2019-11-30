ce2e9056fa741c5114bd70926627601e0a57f5d6
Brittany Javalera <brittjavalera@gmail.com>
2019-11-30 14:07:57 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        
+        @user = User.create(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
+            # binding.pry
         else
             render :new
         end
