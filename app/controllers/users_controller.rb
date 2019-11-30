27a5be3bdc711efbad857382dbd937612876791a
Brittany Javalera <brittjavalera@gmail.com>
2019-11-30 14:12:21 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
+        @user = User.find_by(params[:id])
         if session[:user_id] != @user.id
             redirect_to "/"
         end
